dbb37a96ee3ea6c21ad82a32748dfb7fd1d53834
irvandaibra <irvndibhm354@gmail.com>
2022-01-26 15:25:38 +0700

    belajar javascript tahap 2

---
+++ kalkulator/script.js
@@ -0,0 +1,19 @@
+function insert(num) {
+  document.form.hasil.value = document.form.hasil.value + num;
+}
+
+function equal() {
+  const exp = document.form.hasil.value;
+  if (exp) {
+    document.form.hasil.value = eval(exp);
+  }
+}
+
+function clear() {
+  document.form.hasil.value = "";
+}
+
+function back() {
+  const exp = document.form.hasil.value;
+  document.form.hasil.value = exp.substring(0, exp.length - 1);
+}
